9c4ff746b2ecd1234548e69e38170503cbdd1a07
kuroyukiina <ninjakapog@gmail.com>
2026-09-14 03:59:10 +0700

    fix: update product image urls

--- lib/external.ts
+++ lib/external.ts
@@ -37,26 +37,26 @@ export async function fetchExternal(
       {
         id: '1',
         title: 'Fjallraven - Foldsack No. 1 Backpack',
-        subtitle: '$109.95 • men\'s clothing',
-        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        subtitle: "$109.95 • men's clothing",
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
       },
       {
         id: '2',
         title: 'Mens Casual Premium Slim Fit T-Shirts',
-        subtitle: '$22.3 • men\'s clothing',
-        image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+        subtitle: "$22.3 • men's clothing",
+        image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
       },
       {
         id: '3',
         title: 'Mens Cotton Jacket',
-        subtitle: '$55.99 • men\'s clothing',
-        image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+        subtitle: "$55.99 • men's clothing",
+        image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png',
       },
       {
         id: '4',
         title: 'Mens Casual Slim Fit',
-        subtitle: '$15.99 • men\'s clothing',
-        image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+        subtitle: "$15.99 • men's clothing",
+        image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png',
       },
     ];
   }
